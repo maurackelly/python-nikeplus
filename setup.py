4390ca0bedf9dac72283c68a092ce473f43429fe
Maura Chace <minarets@gmail.com>
2010-11-13 11:35:10 -0500

    Setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nikeplus',
+    version='0.1',
+    author='Maura Chace',
+    url='http://github.com/minarets/python-nikeplus',
+    packages=find_packages(),
+)
